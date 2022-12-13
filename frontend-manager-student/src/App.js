8211ee142f7156a98917978426bbe113633bf41a
tai <nguyentientai10@gmail.com>
2022-12-13 17:06:29 +0700

    #3 setup frontend

--- frontend-manager-student/src/App.js
+++ frontend-manager-student/src/App.js
@@ -6,7 +6,7 @@ import { ToastContainer } from 'react-toastify';
 import RouteDataMain from 'routers/index';
 import './App.css';
 
-vadddr ssfuaaaaanction App() {
+vadddr ssfuaaaaasdasdanction App() {
   return (
     <React.Fragment>
       <Suspense fallback={<Loading />}>
