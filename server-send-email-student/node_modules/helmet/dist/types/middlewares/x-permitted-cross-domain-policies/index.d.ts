c8d9896b4b6b29c9eb0cf07bfdb5df12b2fe69e0
Nguyễn Tiến Tài <63393170+fdhhhdjd@users.noreply.github.com>
2022-12-16 19:17:13 +0700

    delete cache

--- server-send-email-student/node_modules/helmet/dist/types/middlewares/x-permitted-cross-domain-policies/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { IncomingMessage, ServerResponse } from "http"
-export interface XPermittedCrossDomainPoliciesOptions {
-	permittedPolicies?: "none" | "master-only" | "by-content-type" | "all"
-}
-declare function xPermittedCrossDomainPolicies(options?: Readonly<XPermittedCrossDomainPoliciesOptions>): (_req: IncomingMessage, res: ServerResponse, next: () => void) => void
-export default xPermittedCrossDomainPolicies
