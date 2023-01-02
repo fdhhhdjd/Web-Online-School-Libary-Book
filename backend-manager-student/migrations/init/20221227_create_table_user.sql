487656fae946d8f2d58676a84a907e693a159ccd
giabao1511 <giabao712411@gmail.com>
2023-01-02 21:00:15 +0700

    #33 database

--- backend-manager-student/migrations/init/20221227_create_table_user.sql
+++ backend-manager-student/migrations/init/20221227_create_table_user.sql
@@ -1,18 +1,16 @@
 CREATE TABLE public.user (
-    id bigint NOT NULL,
+    user_id bigint NOT NULL,
     created_at timestamp without time zone DEFAULT now() NOT NULL,
     updated_at timestamp without time zone DEFAULT now() NOT NULL,
-    deleted_flg boolean DEFAULT false NOT NULL,
+    isDeleted boolean DEFAULT false NOT NULL,
     name text NOT NULL,
     mssv text NOT NULL,
     avatar_uri text,
+    password text NOT NULL,
+    role smallint DEFAULT '0' :: smallint,
     phone_number text NOT NULL,
     email text,
-    birthday date,
+    dob date,
     address text,
     gender smallint,
-    status smallint DEFAULT '1'::smallint,
-    province_id integer,
-    district_id integer,
-    avatar_uri_temp text
 );
