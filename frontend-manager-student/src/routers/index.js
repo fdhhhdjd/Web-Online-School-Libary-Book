f92e1c494465b3ae0fd3c78802bdf81941d0633f
giabao1511 <giabao712411@gmail.com>
2023-02-24 21:37:18 +0700

    #71 navbar

--- frontend-manager-student/src/routers/index.js
+++ frontend-manager-student/src/routers/index.js
@@ -1,11 +1,11 @@
-import Navbar from 'components/Navbar';
+import Header from 'components/Header';
 
 const RouteDataMain = [
   {
     path: '',
     main: (
       <>
-        <Navbar />
+        <Header />
       </>
     ),
   },
