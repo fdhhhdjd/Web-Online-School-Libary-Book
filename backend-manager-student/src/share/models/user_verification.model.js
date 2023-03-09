e13aaccf577ef854c82c34291f7745e44894f013
Nguyễn Tiến Tài <63393170+fdhhhdjd@users.noreply.github.com>
2023-03-09 14:39:14 +0700

    #180 [Backend] Feature Borrow Book

--- backend-manager-student/src/share/models/user_verification.model.js
+++ backend-manager-student/src/share/models/user_verification.model.js
@@ -10,18 +10,19 @@ module.exports = {
      * @created_at 25/02/2023
      * @description Insert reset verification student
      */
-    insertVerificationEmail: (data) => new Promise((resolve, reject) => {
-        try {
-            const result = knex('verification')
-                .insert(data)
-                .onConflict('verify_id')
-                .merge()
-                .returning(['verify_id']);
-            resolve(result);
-        } catch (error) {
-            reject(error);
-        }
-    }),
+    insertVerificationEmail: (data) =>
+        new Promise((resolve, reject) => {
+            try {
+                const result = knex('verification')
+                    .insert(data)
+                    .onConflict('verify_id')
+                    .merge()
+                    .returning(['verify_id']);
+                resolve(result);
+            } catch (error) {
+                reject(error);
+            }
+        }),
 
     /**
      * @author Nguyễn Tiến Tài
@@ -37,30 +38,32 @@ module.exports = {
      * @created_at 26/02/2023
      * @description Update verification
      */
-    updateVerification: async (data, student_query, return_data) => new Promise((resolve, reject) => {
-        try {
-            const result = knex('verification').update(data).where(student_query).returning(return_data);
-            resolve(result);
-        } catch (error) {
-            reject(error);
-        }
-    }),
+    updateVerification: async (data, student_query, return_data) =>
+        new Promise((resolve, reject) => {
+            try {
+                const result = knex('verification').update(data).where(student_query).returning(return_data);
+                resolve(result);
+            } catch (error) {
+                reject(error);
+            }
+        }),
     /**
      * @author Nguyễn Tiến Tài
      * @created_at 26/02/2023
      * @description Update verification
      */
-    getVerificationList: async () => new Promise((resolve, reject) => {
-        knex('verification')
-            .where('isdeleted', CONSTANTS.DELETED_DISABLE)
-            .where('verified', CONSTANTS.DELETED_DISABLE)
-            .where('check_login', CONSTANTS.DELETED_DISABLE)
-            .orderBy('created_at', 'asc')
-            .then((result) => {
-                resolve(result);
-            })
-            .catch((err) => {
-                reject(err);
-            });
-    }),
+    getVerificationList: async () =>
+        new Promise((resolve, reject) => {
+            knex('verification')
+                .where('isdeleted', CONSTANTS.DELETED_DISABLE)
+                .where('verified', CONSTANTS.DELETED_DISABLE)
+                .where('check_login', CONSTANTS.DELETED_DISABLE)
+                .orderBy('created_at', 'asc')
+                .then((result) => {
+                    resolve(result);
+                })
+                .catch((err) => {
+                    reject(err);
+                });
+        }),
 };
