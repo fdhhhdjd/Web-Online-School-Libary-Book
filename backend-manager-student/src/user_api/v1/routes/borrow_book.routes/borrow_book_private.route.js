21b306010b3ea577af7f575c8791260f6540b67f
Nguyễn Tiến Tài <63393170+fdhhhdjd@users.noreply.github.com>
2023-03-24 13:11:31 +0700

    #180 [Backend] Feature Borrow Book

--- backend-manager-student/src/user_api/v1/routes/borrow_book.routes/borrow_book_private.route.js
+++ backend-manager-student/src/user_api/v1/routes/borrow_book.routes/borrow_book_private.route.js
@@ -7,10 +7,26 @@ const borrowBookController = require('../../controllers/borrow_book.controllers/
 /**
  * @author Nguyễn Tiến Tài
  * @created_at 07/03/2023
- * @updateed_at 08/03/2023
+ * @updated_at 08/03/2023
  * @description Route borrow book
  * @param {('POST')} [method='POST'] The request's method
  */
 router.post('/borrow', borrowBookController.borrowBook);
+
+/**
+ * @author Nguyễn Tiến Tài
+ * @created_at 24/03/2023
+ * @description Route borrow book get all
+ * @param {('GET')} [method='GET'] The request's method
+ */
+router.get('/all', borrowBookController.borrowBookAll);
+
+/**
+ * @author Nguyễn Tiến Tài
+ * @created_at 24/03/2023
+ * @description Route borrow book get detail
+ * @param {('GET')} [method='GET'] The request's method
+ */
+router.get('/detail/:borrowed_book_id', borrowBookController.getDetailBorrowBook);
 
 module.exports = router;
