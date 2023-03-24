ee6e849ed447f728d97f82ef30d80edf0763a3b5
Nguyễn Tiến Tài <63393170+fdhhhdjd@users.noreply.github.com>
2023-03-24 13:20:57 +0700

    #180 [Backend] Feature Borrow Book

--- backend-manager-student/src/admin_api/v1/controllers/borrow_book.controllers/borrow_book.controller.js
+++ backend-manager-student/src/admin_api/v1/controllers/borrow_book.controllers/borrow_book.controller.js
@@ -23,11 +23,7 @@ const BorrowBookController = {
      */
     updateBorrowBook: async (req, res) => {
         const {
-            book_id,
-            user_id,
-            start_date,
-            due_date,
-            status,
+            book_id, user_id, start_date, due_date, status,
         } = req.body.input.borrow_book_input;
 
         // Check input
